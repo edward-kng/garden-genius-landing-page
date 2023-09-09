79e8edc6f753656ca4334061d0793360b39bc853
Edward Kang <edward.kang@protonmail.com>
2023-09-09 16:23:02 +0200

    feat: add home screen with screenshot

---
+++ app/_components/common/nav-bar.tsx
@@ -0,0 +1,14 @@
+import '../../globals.css'
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NavBar() {
+    return (<nav className="w-full h-20 flex flex-row items-center space-x-10 p-4 bg-gg text-gg font-bold text-2xl">
+        <Link href="/" className="flex flex-row space-x-5 items-center">
+            <div className="relative w-14 aspect-square">
+                <Image fill={true} src="/garden-genius-logo.png" alt="Garden Genius Logo"/>
+            </div>
+            <p>Garden Genius</p>
+        </Link>
+    </nav>)
+}
