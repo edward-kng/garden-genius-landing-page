8b089a00d95fbb8e53eec63d8d2c1b7ae9e39926
Edward Kang <edward.kang@protonmail.com>
2023-09-09 17:19:56 +0200

    feat: add feature list

--- app/_components/common/nav-bar.tsx
+++ app/_components/common/nav-bar.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function NavBar() {
-    return (<nav className="w-full h-20 flex flex-row items-center space-x-10 p-4 bg-gg-secondary text-gg font-bold text-2xl">
-        <Link href="/" className="flex flex-row space-x-5 items-center">
-            <div className="relative w-14 aspect-square">
-                <Image fill={true} src="/garden-genius-logo.png" alt="Garden Genius Logo"/>
-            </div>
-            <p>Garden Genius</p>
-        </Link>
-    </nav>)
+  return (
+    <nav className="w-full h-20 flex flex-row items-center space-x-10 p-4 bg-gg-secondary text-gg-primary font-bold text-2xl">
+      <Link href="/" className="flex flex-row space-x-5 items-center">
+        <div className="relative w-14 aspect-square">
+          <Image
+            fill={true}
+            src="/garden-genius-logo.png"
+            alt="Garden Genius Logo"
+          />
+        </div>
+        <p>Garden Genius</p>
+      </Link>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,84 @@
-import Image from 'next/image'
-import Button from './_components/common/button'
-import Link from 'next/link'
+import Image from "next/image";
+import Button from "./_components/common/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center p-8 max-w-6xl mx-auto">
-      <div className='grid grid-cols-5 gap-10'>
+      <div className="grid grid-cols-5 gap-10 gap-y-20 h-auto">
         <div className="col-span-3 space-y-4">
-          <h1 className='text-6xl font-bold'>Garden Genius</h1>
-          <h2 className="text-3xl font-bold">Your personal gardening assistant, right in the palm of your hand!</h2>
-          <div className='space-y-4'>
-            <p>Our mission is to help gardeners of all experience levels cultivate thriving green spaces by leveraging the power of publicly available data.</p>
-            <p>Garden Genius offers tailored watering and planting recommendations based on live weather data and comprehensive information of over 1500 plant species and their growth patterns and ideal environmental conditions.</p>
+          <h1 className="text-6xl font-bold">Garden Genius</h1>
+          <h2 className="text-3xl font-bold">
+            Your personal gardening assistant, right in the palm of your hand!
+          </h2>
+          <div className="space-y-4">
+            <p>
+              Our mission is to help gardeners of all experience levels
+              cultivate thriving green spaces by leveraging the power of
+              publicly available data.
+            </p>
+            <p>
+              Garden Genius offers tailored watering and planting
+              recommendations based on live weather data and comprehensive
+              information of over 1500 plant species and their growth patterns
+              and ideal environmental conditions.
+            </p>
           </div>
-          <div className='flex flex-row space-x-4'>
+          <div className="flex flex-row space-x-4">
             <Button disabled={true}>Download (Coming Soon!)</Button>
             <Button>View Screenshots</Button>
           </div>
         </div>
         <div className="relative col-span-2">
-          <Image className="rounded-lg border-black border-4 border-gray-900 overflow-hidden" src="/screenshot-home-screen.png" height={1000} width={1000} alt="Screenshot of home screen" style={{height: "70%", width: "auto"}} />
+          <Image
+            className="rounded-lg border-black border-4 border-gray-900 overflow-hidden"
+            src="/screenshot-home-screen.png"
+            alt="Screenshot of home screen"
+            height={1000}
+            width={1000}
+            style={{ height: "auto", width: "70%" }}
+          />
+        </div>
+        <div className="col-span-2 relative w-full">
+          <Image
+            src="/about-illustration.png"
+            alt="Male and female gardener planting pot plants"
+            height={1000}
+            width={1000}
+            style={{ height: "auto", width: "100%" }}
+          />
+        </div>
+        <div className="col-span-3 flex flex-col space-y-10">
+          <h1 className="text-4xl font-bold">Features</h1>
+          <div className="bg-gg-secondary p-8 rounded-lg w-full text-gg-secondary">
+            <p>
+              <b>Extensive Plant Database:</b> Access information on countless
+              plant species, including growth habits, preffered climate and care
+              requirements.
+            </p>
+            <p>
+              <b>Personalised Recommendations:</b> Receive customised watering
+              and planting suggestions based on your garden's specific
+              conditions and your chosen plants.
+            </p>
+            <p>
+              <b>Location-based Insights:</b> Harness the power of local climate
+              data to optimise your garden's performance, adapting your approach
+              according to seasonal changes.
+            </p>
+            <p>
+              <b>Intuitive Interface:</b> Navigate through our user-friendly
+              platform to quickly find the information you need to implement our
+              expert advice.
+            </p>
+            <p>
+              <b>Community Connection:</b> Join a network of fellow garden
+              enthusiasts and exchange knowledge, tips and experiences through
+              our integrated forum.
+            </p>
+          </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
