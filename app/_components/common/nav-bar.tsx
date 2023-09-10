5c08ffd199b652d2553b537f862e0c23eca70505
Edward Kang <edward.kang@protonmail.com>
2023-09-10 18:26:37 +0200

    fix: fix navbar disappearing behind dialog box

--- app/_components/common/nav-bar.tsx
+++ app/_components/common/nav-bar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="fixed bg-white w-full h-20 flex flex-row items-center space-x-10 p-4 text-gg-primary font-bold text-2xl border-b border-gray-300">
+    <nav className="fixed z-50 bg-white w-full h-20 flex flex-row items-center space-x-10 p-4 text-gg-primary font-bold text-2xl border-b border-gray-300">
       <Link href="/" className="flex flex-row space-x-5 items-center">
         <div className="relative w-14 aspect-square">
           <Image
