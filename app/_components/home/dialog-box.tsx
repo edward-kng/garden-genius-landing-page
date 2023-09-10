27a52e10cdaa146436cf612c815c0fc6e19653b4
Edward Kang <edward.kang@protonmail.com>
2023-09-10 22:27:56 +0200

    feat: add visual feedback to email form

--- app/_components/home/dialog-box.tsx
+++ app/_components/home/dialog-box.tsx
@@ -8,39 +8,55 @@ interface DialogBoxAttributes extends HTMLAttributes<HTMLDivElement> {
 
 export default function DialogBox({ onExit, className }: DialogBoxAttributes) {
   const [value, setValue] = useState("");
+  const [success, setSuccess] = useState(false);
 
   return (
-    <div className={`${className} bg-white rounded-lg border-2 p-16`}>
-      <form
-        className="flex flex-col space-y-4 items-center"
-        onSubmit={(evt) => {
-          evt.preventDefault();
-          console.log(value);
-        }}
-      >
-        <h1 className="text-4xl">Join Waiting List</h1>
-        <p>Enter your email and we&apos;ll notify you when the beta is out.</p>
-        <InputField
-          value={value}
-          onChange={(evt) => {
-            setValue(evt.target.value);
+    <div
+      className={`${className} bg-white rounded-lg border-2 p-16 flex flex-col items-center space-y-4`}
+    >
+      <h1 className="text-4xl">Join Waiting List</h1>
+      {success ? (
+        <p>Registration successful!</p>
+      ) : (
+        <form
+          className="flex flex-col space-y-4 items-center"
+          onSubmit={(evt) => {
+            evt.preventDefault();
+            setSuccess(true);
+            setTimeout(() => {
+              setSuccess(false);
+              setValue("");
+              onExit();
+            }, 1000);
           }}
-          placeholder="Email"
-        />
-        <div className="grid grid-cols-2 gap-2 w-full">
-          <Button
-            type="button"
-            variant="transparent"
-            className="w-full"
-            onClick={onExit}
-          >
-            Cancel
-          </Button>
-          <Button type="submit" className="w-full">
-            Sign Me Up
-          </Button>
-        </div>
-      </form>
+        >
+          <p>
+            Enter your email and we&apos;ll notify you when the beta is out.
+          </p>
+          <InputField
+            type="email"
+            value={value}
+            required={true}
+            onChange={(evt) => {
+              setValue(evt.target.value);
+            }}
+            placeholder="Email"
+          />
+          <div className="grid grid-cols-2 gap-2 w-full">
+            <Button
+              type="button"
+              variant="transparent"
+              className="w-full"
+              onClick={onExit}
+            >
+              Cancel
+            </Button>
+            <Button type="submit" className="w-full">
+              Sign Me Up
+            </Button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
