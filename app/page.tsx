3208cc73052a1b97b42db7e82e7da298e594ce74
Edward Kang <edward.kang@protonmail.com>
2023-09-10 07:47:00 +0200

    feat: implement responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Button from "./_components/common/button";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center p-8 max-w-6xl mx-auto">
-      <div className="grid grid-cols-5 gap-10 gap-y-20 h-auto">
-        <div className="col-span-3 space-y-4">
+      <div className="md:grid md:grid-cols-5 grid-cols-1 gap-10 space-y-10 h-auto">
+        <div className="md:col-span-3 space-y-4">
           <h1 className="text-6xl font-bold">Garden Genius</h1>
           <h2 className="text-3xl font-bold">
             Your personal gardening assistant, right in the palm of your hand!
@@ -28,7 +28,7 @@ export default function Home() {
             <Button>View Screenshots</Button>
           </div>
         </div>
-        <div className="relative col-span-2">
+        <div className="relative md:col-span-2 flex flex-col items-center">
           <Image
             className="rounded-lg border-black border-4 border-gray-900 overflow-hidden"
             src="/screenshot-home-screen.png"
@@ -38,7 +38,7 @@ export default function Home() {
             style={{ height: "auto", width: "70%" }}
           />
         </div>
-        <div className="col-span-2 relative w-full">
+        <div className="md:col-span-2 relative w-full">
           <Image
             src="/about-illustration.png"
             alt="Male and female gardener planting pot plants"
@@ -47,7 +47,7 @@ export default function Home() {
             style={{ height: "auto", width: "100%" }}
           />
         </div>
-        <div className="col-span-3 flex flex-col space-y-10">
+        <div className="md:col-span-3 flex flex-col space-y-10">
           <h1 className="text-4xl font-bold">Features</h1>
           <div className="bg-gg-secondary p-8 rounded-lg w-full text-gg-secondary">
             <p>
@@ -77,32 +77,32 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="col-span-5 text-center flex flex-col space-y-4">
+        <div className="col-span-5 text-center flex flex-col space-y-4 items-center">
           <p className="text-3xl">Powered By</p>
-          <div className="relative flex flex-row space-x-20 justify-center items-center p-8">
+          <div className="relative grid md:grid-cols-4 grid-cols-2 justify-center items-center p-8">
             <Image
-              className="h-10 w-auto"
+              className="h-5 w-auto"
               height={1000}
               width={1000}
               src="https://kotlinlang.org/docs/images/kotlin-logo.png"
               alt="Kotlin logo"
             />
             <Image
-              className="h-20 w-auto"
+              className="h-10 w-auto"
               height={1000}
               width={1000}
               src="https://upload.wikimedia.org/wikipedia/commons/3/38/SQLite370.svg"
               alt="Kotlin logo"
             />
             <Image
-              className="h-20 w-auto"
+              className="h-10 w-auto"
               height={1000}
               width={1000}
               src="https://www.python.org/static/community_logos/python-logo-generic.svg"
               alt="Kotlin logo"
             />
             <Image
-              className="h-20 w-auto"
+              className="h-10 w-auto"
               height={1000}
               width={1000}
               src="https://www.met.no/en/About-us/logo/_/image/943fbdc6-eba8-4e19-aff1-75f453ba9c7f:4bbfe4ae9e1826b3e159a3fff6e5d3893a93b072/width-768/Met_RGB_Horisontal_ENG.jpg"
